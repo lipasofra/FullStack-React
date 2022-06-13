e8f11d9f728086aa8986d26a4d4d368da840e39d
Lina Soler <lipasofra.ph@gmail.com>
2022-06-13 17:51:56 -0500

    all course one object, exercise 1.5

--- src/App.js
+++ src/App.js
@@ -33,8 +33,9 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React', 
       exercises: 10
@@ -48,12 +49,13 @@ const App = () => {
       exercises: 14
     }
   ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
