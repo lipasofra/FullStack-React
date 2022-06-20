85100e0c63d234eb796e05ee98682c4b5d1c0b4a
Lina Soler <lipasofra.ph@gmail.com>
2022-06-20 18:39:55 -0500

    Start Unicafe Feedback exercise 1.6

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
-const Header = (props) => {
-  return(
-    <h1>{props.course}</h1>
-  )
+const Button = (props) => (
+  <button onClick = {props.handleClick}>
+    {props.text}
+  </button>
+)
   
-}
-
-const Part = (props) => {
-  return(
-    <p>{props.part} {props.exercise}</p>
-  )
-  
-}
-
-const Content = (props) => {
-  return(
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-  </div>
-  )
-}
-
-const Total = (props) => {
-  const total = props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React', 
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
+    <>
+      <h1>Unicafe Feedback</h1>
+      
+      <Button handleClick={()=>setGood(good+1)} text="Good" />
+      <Button handleClick={()=>{setNeutral(neutral+1)}} text="Neutral" />
+      <Button handleClick={()=>{setBad(bad+1)}} text="Bad" />
+      
+      <h2>Feedback Results</h2>
+      <h3>Good: {good}</h3>
+      <h3>Bad: {bad}</h3>
+      <h3>Neutral: {neutral}</h3>
+    </>
+    
   )
 }
 
